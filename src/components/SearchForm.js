c0377e67736ee3eb251fa6b16bc989c3eb0a21f4
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-28 19:42:05 -0400

    added components to search and login

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Form, Field, withFormik } from "formik";
+import BookList from "./BookList";
+import * as Yup from "yup";
+import styled from "styled-components";
 
-export default function SearchForm({ onSearch }) {
-  const [search, setSearch] = useState({ bookname: "" });
-  const handleInputChange = event => {
-    setSearch({ ...search, bookname: event.target.value });
-  };
+const SearchDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SearchForm = ({ errors, touched, values, isSubmitting, status }) => {
+  const [info, setInfo] = useState([]);
+  console.log("this is touched", touched);
+  useEffect(() => {
+    if (status) {
+      setInfo([...info, status]);
+    }
+  }, [info, status]);
+
   return (
-    <section className="search-form">
-      <form onSubmit={() => onSearch(search.bookname)}>
-        <input
-          onChange={handleInputChange}
-          placeholder="bookname"
-          value={search.bookname}
-          bookname="bookname"
+    <SearchDiv>
+      <h1>What kind of book are you looking for?</h1>
+      <Form>
+        <Field
+          component="textarea"
+          type="text"
+          name="bookdesc"
+          placeholder="Book Description"
+          min-width="500px"
         />
-        <button type="submit">Search</button>
-      </form>
-    </section>
+        {touched.bookdesc && errors.booksdesc && <p>{errors.bookdesc}</p>}
+      </Form>
+      <Text>{JSON.stringify(values)}</Text>
+      <button type="submit" disabled={isSubmitting}>
+        Submit!
+      </button>
+
+      <BookList />
+    </SearchDiv>
   );
-}
+};
+
+const FormikSearchForm = withFormik({
+  mapPropsToValues({ bookdesc }) {
+    return {
+      booksdesc: bookdesc || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    bookdesc: Yup.string().required("A description is required.")
+  }),
+
+  handleSubmit(values, { setStatus, resetForm, setErrors, setSubmitting }) {
+    axios
+
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log(res);
+        setStatus(res.data);
+        resetForm();
+        setSubmitting(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setSubmitting(false);
+      });
+  }
+})(SearchForm);
+export default FormikSearchForm;
