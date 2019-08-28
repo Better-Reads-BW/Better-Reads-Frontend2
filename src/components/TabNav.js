574097f0cbb6d808152afd6820cde6a9ec1003fc
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-28 14:51:21 -0400

    added tabnav

---
+++ src/components/TabNav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Tab, Menu, Icon } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+
+const Nav = props => <NavLink exact {...props} activeClassName="active" />;
+
+const createLabel = (iconName, labelText) => (
+  <span>
+    <Icon name={iconName} />
+    {labelText}
+  </span>
+);
+
+const welcomeLabel = createLabel("home", "Home Page");
+const signinLabel = createLabel("signin", "Signin Page");
+const signupLabel = createLabel("signup", "SignUp Page");
+
+const panes = [
+  {
+    menuItem: <Menu.Item key="home" as={Nav} to={`/`} content={welcomeLabel} />
+  },
+  {
+    menuItem: <Menu.Item key="signup" as={Nav} to={"/"} content={signupLabel} />
+  },
+  {
+    menuItem: <Menu.Item key="signin" as={Nav} to={"/"} content={signinLabel} />
+  }
+];
+const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />;
+
+export default TabNav;
