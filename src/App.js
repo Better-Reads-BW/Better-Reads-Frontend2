aa25aea3b9b06d6687c4674797aa00215ca69241
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-08-28 21:28:42 -0500

    copied most files from my branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Link } from "react-router-dom";  
+
+//vanesa components
+import LoginForm from './components/LoginForm.js'
+import Home from './components/Home.js'
+import SignupForm from './components/SignupForm.js'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login!</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup!</Link>
+        </li>
+        <li>
+          <Link to="/protected">Users Only</Link>
+        </li>
+      </ul>
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/signup" component={SignupForm} />
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/protected" /*component={UserPage}*/ />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Homepage = () => {
+    return(
+        <div>
+            <h1>Welcome.</h1>
+        </div>
+    )
+}
+
+export default Homepage
